4182c24f3d6f9e8172fb48abf1c70124a6c2e80a
Jenn Gonzalez <jgonz9873@gmail.com>
2019-08-20 23:47:06 -0700

    chained trefle API calls to get queried plants by Id

--- src/trefle/trefle-api.js
+++ src/trefle/trefle-api.js
@@ -1,24 +1,41 @@
 const config = require('../config')
-const request = require('request')
 const rp = require('request-promise')
+const request = require('request')
+
 
 trefleApi = (req, res) => {
     const trefleUrl = config.TREFLE_URL
     const trefleKey = process.env.TREFLE_API_KEY
     const searchTerm  = req.query.q
-    console.log(searchTerm)
     rp({
         uri: `${trefleUrl}?token=${trefleKey}&q=${searchTerm}`,
         json: true
     })
-        .then(data => {
-            console.log(data)
-            res.send(data)
+        .then(data =>  new Promise(function(resolve, reject) {
+            const userPlants = []
+            data.forEach(plant => {
+                rp({
+                    uri: `${trefleUrl}/${plant.id}?token=${trefleKey}`,
+                    json: true
+                })
+                .then(plantData => {
+                    userPlants.push(plantData)
+                }) .catch(err => {
+                    console.log(err)
+                })
+            })
+            setTimeout(() => {
+                resolve(userPlants)
+            }, 1000)
+            return Promise.all(userPlants)
+        }))
+        .then(userPlants => {
+            res.send(userPlants[1])
         })
-        .catch(err => {
-            console.log(err)
-        })
+        .catch(err => console.log(err))
 }
-        
+
+
+    
 
 module.exports = trefleApi
