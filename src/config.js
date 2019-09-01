52b48e6a37692e9f9e3616dffd90b2f8e0d6ca1d
Jenn Gonzalez <jgonz9873@gmail.com>
2019-09-01 14:54:00 -0700

    changed to DATABASE_URL in config for heroku

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://greenhouse_admin@localhost/greenhouse',
+    DB_URL: process.env.DATABASE_URL || 'postgresql://greenhouse_admin@localhost/greenhouse',
     JWT_SECRET: process.env.JWT_SECRET || 'WBbDbNXhTLxDkzz63PsZAIIIjakdE7IODHTmujkuTmsZfJyzXA1ICYCg5fcVwDN8f/aGBmidsZRU7e4YiasTK28rvX71o969H+dfEppcVauhEY+3P87en0TriMR+Q+Gls3IKZhwTbEu0ptdynmTbNBqsCMWLa+TU7S+khFcOIvDsAed0vr5vVrAHEULBTEeEtJrColFHIdvaThWOasq8hxsH2mMDh3Hxz1r/Eu673VJowUMxn29oZmDVnsNAKPmg8wAa4aCrdj5TNfBEx/Ya1q1JhuAeJX2AIQ4Bx1ik6iWeHQxcDBeP2+y+579kpQRI8O8uch//G53hFxPkviJceg==',
     JWT_EXPIRY: process.env.JWT_EXPIRY || '5h',
     CLIENT_ORIGIN: process.env.CLIENT_ORIGIN ||'http://localhost:3000',
